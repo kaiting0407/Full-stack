86e691badb14aa0b08fa273e318cc0b8c05a2262
kaiting0407 <mike26593643@gmail.com>
2024-03-30 16:59:45 +0800

    first

---
+++ JS/app.js
@@ -0,0 +1,65 @@
+// console.log("hellow world");
+// let radius = prompt("請輸入r");
+// function circle(r) {
+//   return r * r * 3.14;
+// }
+// alert(circle(radius));
+/*let mike = {
+  firstname: "chou",
+  age: 20,
+  married: false,
+  sayHi() {
+    console.log(this.firstname + "says hi");
+  },
+  sayWord(word) {
+    console.log("mike says " + word);
+  },
+};
+mike.sayHi();
+mike.sayWord("123");
+let sum=0;
+for (let i = 0; i <10;i++) {
+    sum++;
+}*/
+//let pi=Math.pi;
+/*let answer = Math.floor(Math.random() * 100);
+let floor = 0;
+let ceil = 99;
+while (true) {
+  let guess = prompt("猜數字在" + floor + "~" + ceil + "之間");
+  if (guess == answer) {
+    alert("true");
+    break;
+  } else if (guess > ceil || guess < floor) {
+    alert("超出範圍");
+    continue;
+  } else if (guess < answer) {
+    floor = guess;
+  } else if(guess > answer) {
+    ceil = guess;
+  }
+}*/
+/*const friends = ["Harry", "Ron", "Snap", "Mike", "Grace"];
+const reversed_friends = [];
+let length = friends.length;
+for (let i=0;i<length;i++) {
+    reversed_friends[i]=friends[length-1-i];
+ }
+ function findBiggest(arr) {
+  length = arr.length;
+  let biggestNumber=0;
+  for (let i =0;i<length;i++) {
+    if(arr[i] > biggestNumber){
+      biggestNumber=arr[i];
+    }
+  }  
+  return biggestNumber;
+}
+function addUpTo(n) {
+  let result=0;
+  for(let i=1;i<=n;i++){
+    result+=i;
+  }
+  return result;
+}*/
+console.log("11");
---
+++ JS/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function addUpTo(n) {
+  let result=0;
+  for(let i=1;i<=n;i++){
+    result+=i;
+  }
+  return result;
+}
